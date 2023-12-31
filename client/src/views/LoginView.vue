755ea91e9e764e57b9419ed0c003ed889c86f3f2
Misael Abanto <josemisaelabanto@gmail.com>
2023-12-31 18:08:01 -0500

    feat: login loading

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -4,6 +4,7 @@
       :model-value="profile"
       save-button-name="Ingresar"
       @save="saveUser"
+      :loading="loading"
     />
   </div>
 </template>
@@ -12,13 +13,16 @@
 import EditProfile from '@/components/molecules/EditProfile.vue'
 import type { User } from '@/interfaces/user'
 import { useProfileStore } from '@/stores/profile'
+import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
 const { profile, createUser, saveProfile } = useProfileStore()
+const loading = ref<boolean>(false)
 
 const saveUser = async (user: Omit<User, 'id'>) => {
+  loading.value = true
   const { data } = await createUser({
     emoji: user.emoji,
     name: user.name
@@ -26,6 +30,7 @@ const saveUser = async (user: Omit<User, 'id'>) => {
   if (data.value) {
     saveProfile(data.value)
   }
+  loading.value = false
   router.push({ name: 'queue' })
 }
 </script>
