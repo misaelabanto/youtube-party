f19aa355a98d4fcaae081b82d4f490c7ff4f10d4
Misael Abanto <josemisaelabanto@gmail.com>
2023-12-31 01:15:52 -0500

    feat: edit profile working

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,7 +1,21 @@
 <template>
-  <EditProfile />
+  <EditProfile
+    :model-value="profile"
+    save-button-name="Guardar"
+    @save="updateUser"
+  />
 </template>
 
 <script setup lang="ts">
 import EditProfile from '@/components/molecules/EditProfile.vue'
+import type { User } from '@/interfaces/user'
+import { useProfileStore } from '@/stores/profile'
+import { storeToRefs } from 'pinia'
+
+const profileStore = useProfileStore()
+const { profile } = storeToRefs(profileStore)
+
+const updateUser = (user: Omit<User, '_id'>) => {
+  profileStore.updateUser(profile.value._id, user)
+}
 </script>
