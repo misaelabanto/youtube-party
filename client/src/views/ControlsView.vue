0f7bb45ac48238b21a423b4b808a4b5052b7ed03
Misael Abanto <josemisaelabanto@gmail.com>
2023-12-31 18:07:53 -0500

    feat: controls view

---
+++ client/src/views/ControlsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <PlayerActions
+    @pause="send('pause')"
+    @play="send('play')"
+    @next="send('next')"
+    @previous="send('previous')"
+  />
+</template>
+
+<script setup lang="ts">
+import PlayerActions from '@/components/molecules/PlayerActions.vue'
+import { useWebSocket } from '@vueuse/core'
+
+const { send } = useWebSocket(
+  `${import.meta.env.VITE_API_URL.replace('http', 'ws')}/ws`
+)
+</script>
