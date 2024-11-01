cd33d2afcc7b0b8785986fef39a78d772fccc9e9
Misael Abanto <josemisaelabanto@gmail.com>
2024-10-31 21:24:38 -0500

    feat(backend/server): add interface for track and current track data
    - Introduced new TypeScript interfaces for track data representation
    - Defined structures for album, artist, images, and external URLs
    - Enabled strong typing for music track logic and current playing track

---
+++ server/src/interfaces/track.ts
@@ -0,0 +1,64 @@
+interface ExternalUrls {
+	spotify: string;
+}
+
+interface Artist {
+	external_urls: ExternalUrls;
+	href: string;
+	id: string;
+	name: string;
+	type: string;
+	uri: string;
+}
+
+interface Image {
+	height: number;
+	url: string;
+	width: number;
+}
+
+interface Album {
+	album_type: string;
+	artists: Artist[];
+	external_urls: ExternalUrls;
+	href: string;
+	id: string;
+	images: Image[];
+	is_playable: boolean;
+	name: string;
+	release_date: string;
+	release_date_precision: string;
+	total_tracks: number;
+	type: string;
+	uri: string;
+}
+
+interface ExternalIds {
+	isrc: string;
+}
+
+export interface Track {
+	album: Album;
+	artists: Artist[];
+	disc_number: number;
+	duration_ms: number;
+	explicit: boolean;
+	external_ids: ExternalIds;
+	external_urls: ExternalUrls;
+	href: string;
+	id: string;
+	is_local: boolean;
+	is_playable: boolean;
+	name: string;
+	popularity: number;
+	preview_url: string;
+	track_number: number;
+	type: string;
+	uri: string;
+}
+
+export interface CurrentTrack {
+	is_playing: boolean;
+	item: Track;
+	progress_ms?: number;
+}
